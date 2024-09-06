bfe242b02d4faf9c1a6fb1d15f0dd66d3286a287
jcsteinmetz <jcsteinmetz@gmail.com>
2024-09-06 18:12:32 -0400

    Wire includes input and output

--- circuit_builder.py
+++ circuit_builder.py
@@ -11,21 +11,18 @@ class GridCanvas(QWidget):
         self.status = "grab"  # Track which button is toggled
         self.last_mouse_pos = None  # Track the last mouse position for dragging
         self.offset = QPointF(0, 0)  # Offset for grid dragging
-        self.update_cursor()
         self.show_floating_image = False  # Track if square should be shown
         self.floating_image_pos = QPointF(0, 0)  # Position of the square
         self.setMouseTracking(True)
         self.placed_components = []  # List to store placed components
+        self.wires = []  # List to store wires with their start and end positions
+        self.update_cursor()
 
     def update_cursor(self):
         if self.status == "grab":
             self.setCursor(Qt.SizeAllCursor)
-        elif self.status == "input":
-            self.setCursor(Qt.BlankCursor)
-        elif self.status == "output":
-            self.setCursor(Qt.BlankCursor)
-        elif self.status == "wire":
-            self.setCursor(Qt.BlankCursor)
+        elif self.status in ["wire_start", "wire_end"]:
+            self.setCursor(Qt.CrossCursor)
         else:
             self.setCursor(Qt.ArrowCursor)
 
@@ -35,7 +32,8 @@ class GridCanvas(QWidget):
         # Fill the canvas with the background color
         painter.fillRect(self.rect(), QColor(255, 255, 255))
 
-        painter.setPen(QColor(200, 200, 200))  # Light gray color for grid lines
+        # Light gray color for grid lines
+        painter.setPen(QColor(200, 200, 200))
 
         # Apply zoom factor to grid size
         zoomed_grid_size = self.grid_size * self.zoom_factor
@@ -56,36 +54,62 @@ class GridCanvas(QWidget):
         for component in self.placed_components:
             self.draw_component(painter, component['type'], component['pos'])
 
+        # Draw wires
+        for wire in self.wires:
+            self.draw_wire(painter, wire)
+
         # Draw a floating image if needed
         if self.show_floating_image:
             self.draw_floating_image()
 
+    def draw_wire(self, painter, wire):
+        if wire['start'] and wire['end']:
+            pen = QPen(QColor(0, 0, 0, 100))
+            pen.setWidth(5)  # Set the width of the line (you can adjust this value)
+            painter.setPen(pen)
+            painter.drawLine(QPointF(wire['start'].x(), wire['start'].y()), QPointF(wire['end'].x(), wire['end'].y()))
+
     def draw_component(self, painter, comp_type, pos):
-        if comp_type == "input":
-            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
-            painter.drawRect(pos.x() - 50*self.zoom_factor, pos.y() - 50*self.zoom_factor, 100*self.zoom_factor, 100*self.zoom_factor)  # 100x100 square
-        elif comp_type == "output":
-            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
-            painter.drawRect(pos.x() - 50*self.zoom_factor, pos.y() - 50*self.zoom_factor, 100*self.zoom_factor, 100*self.zoom_factor)  # 100x100 square
-        elif comp_type == "wire":
+        if comp_type == "wire_start":
             pen = QPen(QColor(0, 0, 255, 100))
             pen.setWidth(5)  # Set the width of the line (you can adjust this value)
             painter.setPen(pen)
-            painter.drawLine(QPoint(0, pos.y()), QPoint(self.width(), pos.y()))
+            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
+            painter.drawRect(pos.x() - 50 * self.zoom_factor, pos.y() - 50 * self.zoom_factor, 100 * self.zoom_factor, 100 * self.zoom_factor)  # 100x100 square
+
+        elif comp_type == "wire_end":
+            # Drawing wires is handled separately
+            pen = QPen(QColor(255, 0, 0, 100))
+            pen.setWidth(5)  # Set the width of the line (you can adjust this value)
+            painter.setPen(pen)
+            painter.setBrush(QColor(255, 0, 0, 100))  # Semi-transparent blue color
+            painter.drawRect(pos.x() - 50 * self.zoom_factor, pos.y() - 50 * self.zoom_factor, 100 * self.zoom_factor, 100 * self.zoom_factor)  # 100x100 square
 
     def draw_floating_image(self):
         painter = QPainter(self)
-        if self.status == "input":
-            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
-            painter.drawRect(self.floating_image_pos.x() - 50*self.zoom_factor, self.floating_image_pos.y() - 50*self.zoom_factor, 100*self.zoom_factor, 100*self.zoom_factor)  # 100x100 square
-        elif self.status == "output":
-            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
-            painter.drawRect(self.floating_image_pos.x() - 50*self.zoom_factor, self.floating_image_pos.y() - 50*self.zoom_factor, 100*self.zoom_factor, 100*self.zoom_factor)  # 100x100 square
-        elif self.status == "wire":
+        if self.status == "wire_start":
             pen = QPen(QColor(0, 0, 255, 100))
             pen.setWidth(5)  # Set the width of the line (you can adjust this value)
             painter.setPen(pen)
-            painter.drawLine(QPoint(0,self.floating_image_pos.y()), QPoint(self.width(),self.floating_image_pos.y()))
+            painter.setBrush(QColor(0, 0, 255, 100))  # Semi-transparent blue color
+            painter.drawRect(self.floating_image_pos.x() - 50 * self.zoom_factor, self.floating_image_pos.y() - 50 * self.zoom_factor, 100 * self.zoom_factor, 100 * self.zoom_factor)  # 100x100 square
+
+        elif self.status == "wire_end":
+            if self.wires:
+                wire_start_pos = self.wires[-1]['start']
+                wire_end_pos = self.floating_image_pos
+
+                pen = QPen(QColor(0, 0, 0, 100))
+                pen.setWidth(5)  # Set the width of the line (you can adjust this value)
+                painter.setPen(pen)
+                painter.drawLine(QPointF(wire_start_pos.x(), wire_start_pos.y()), QPointF(wire_end_pos.x(), wire_start_pos.y()))
+
+                pen = QPen(QColor(255, 0, 0, 100))
+                pen.setWidth(5)  # Set the width of the line (you can adjust this value)
+                painter.setPen(pen)
+                painter.setBrush(QColor(255, 0, 0, 100))  # Semi-transparent red color
+                painter.drawRect(wire_end_pos.x() - 50 * self.zoom_factor, wire_start_pos.y() - 50 * self.zoom_factor, 100 * self.zoom_factor, 100 * self.zoom_factor)  # 100x100 square
+
 
     def wheelEvent(self, event: QWheelEvent):
         # Get the mouse position relative to the widget
@@ -109,6 +133,14 @@ class GridCanvas(QWidget):
                 new_distance_to_mouse = distance_to_mouse * (new_zoom_factor / self.zoom_factor)
                 comp['pos'] = mouse_pos + new_distance_to_mouse
 
+            for wire in self.wires:
+                distance_from_start_to_mouse = wire['start'] - mouse_pos
+                distance_from_end_to_mouse = wire['end'] - mouse_pos
+                new_distance_from_start_to_mouse = distance_from_start_to_mouse * (new_zoom_factor / self.zoom_factor)
+                new_distance_from_end_to_mouse = distance_from_end_to_mouse * (new_zoom_factor / self.zoom_factor)
+                wire['start'] = mouse_pos + new_distance_from_start_to_mouse
+                wire['end'] = mouse_pos + new_distance_from_end_to_mouse
+
             # Adjust the offset to keep the grid under the mouse stationary
             self.offset += (mouse_pos_before_zoom - mouse_pos_after_zoom) * self.grid_size * new_zoom_factor
 
@@ -120,10 +152,29 @@ class GridCanvas(QWidget):
 
     def mousePressEvent(self, event: QMouseEvent):
         if event.button() == Qt.LeftButton:
-            if self.status in ["input", "output", "wire"]:
-                # Place a copy of the floating image
+            if self.status == "wire_start":
+                # Place the start of the wire
                 self.placed_components.append({'type': self.status, 'pos': self.floating_image_pos})
+                self.wires.append({'start': self.floating_image_pos, 'end': None})  # Start of the wire
                 self.update()  # Redraw to show the new component
+
+                self.status = "wire_end"
+                
+                # Make floating image disappear until mouse moves
+                self.show_floating_image = False
+
+            elif self.status == "wire_end":
+                # Update the end of the wire
+                wire_end_pos = QPointF(self.floating_image_pos.x(), self.placed_components[-1]['pos'].y())
+                self.placed_components.append({'type': self.status, 'pos': wire_end_pos})
+                if self.wires:
+                    self.wires[-1]['end'] = wire_end_pos  # End of the wire
+                self.update()  # Redraw to show the new component
+
+                self.status = "wire_start"
+
+                # Make floating image disappear until mouse moves
+                self.show_floating_image = False
 
             elif self.status == "grab":
                 self.last_mouse_pos = event.position()
@@ -137,11 +188,17 @@ class GridCanvas(QWidget):
             # Move placed components
             for comp in self.placed_components:
                 comp['pos'] += delta
+            for wire in self.wires:
+                wire['start'] += delta
+                wire['end'] += delta
 
             self.last_mouse_pos = event.position()  # Update the last mouse position
             self.update()  # Redraw the grid with the new offset
 
-        # Update the floating image's position if "Input" mode is active
+        # Make floating image show whenever mouse moves
+        self.show_floating_image = True
+
+        # Update the floating image's position
         mouse_pos = event.position()
         # Calculate the grid-aligned position
         zoomed_grid_size = self.grid_size * self.zoom_factor
@@ -160,7 +217,7 @@ class GridCanvas(QWidget):
 
     def enterEvent(self, event):
         self.update_cursor()  # Update cursor on entering the widget
-        if self.status in ["input", "output", "wire"]:
+        if self.status in ["wire_start", "wire_end"]:
             self.show_floating_image = True  # Show floating image when entering the canvas
         self.update()
 
@@ -193,8 +250,6 @@ class MainWindow(QMainWindow):
 
         # Create tool/button labels and actions
         tool_labels = [("Grab", self.grab_action),
-                         ("Input", self.input_action),
-                         ("Output", self.output_action),
                          ("Wire", self.wire_action)]
 
         button_labels = [("Clear", self.clear_action),
@@ -241,24 +296,11 @@ class MainWindow(QMainWindow):
         self.canvas.status = "grab"  # Enable grab mode
         self.canvas.update_cursor()  # Update cursor
         self.canvas.show_floating_image = False  # Hide the floating image
-    
-    def input_action(self):
-        print("Input selected")
-        self.canvas.status = "input"  # Disable grab mode for wire placement
-        self.canvas.update_cursor()  # Update cursor
-        self.canvas.show_floating_image = True  # Show the floating image
-
-    def output_action(self):
-        self.canvas.update()
-        print("Output selected")
-        self.canvas.status = "output"  # Disable grab mode for wire placement
-        self.canvas.update_cursor()  # Update cursor
-        self.canvas.show_floating_image = True  # Show the floating image
 
     def wire_action(self):
         self.canvas.update()
         print("Wire selected")
-        self.canvas.status = "wire"  # Disable grab mode for wire placement
+        self.canvas.status = "wire_start"  # Disable grab mode for wire placement
         self.canvas.update_cursor()  # Update cursor
         self.canvas.show_floating_image = True  # Show the floating image
 
@@ -266,6 +308,7 @@ class MainWindow(QMainWindow):
         self.canvas.update()
         print("Clear selected")
         self.canvas.placed_components = []
+        self.canvas.wires = []
 
     def quit_action(self):
         self.canvas.update()
