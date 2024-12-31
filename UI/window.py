1861a8fd688002d1b3a129f6cc0bd49644da7582
jcsteinmetz <jcsteinmetz@gmail.com>
2024-12-31 02:11:53 -0500

    console label

--- UI/window.py
+++ UI/window.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QMainWindow, QWidget, QSplitter, QVBoxLayout
+from PySide6.QtWidgets import QMainWindow, QWidget, QSplitter, QVBoxLayout, QLabel
 from PySide6.QtCore import Qt
 from canvas import Canvas
 from console import Console
@@ -11,13 +11,14 @@ class MainWindow(QMainWindow):
 
         # Main widgets
         self.control_panel = ControlPanel(self)
+        self.console_label = QLabel("Console")
         self.console = Console(self)
         self.canvas = Canvas(self)
         toolbar = ToolBar(self)
 
         # Create central widget layout
         splitter = QSplitter(Qt.Horizontal)
-        left_widget = self.qvbox_widget([self.control_panel, self.console])
+        left_widget = self.qvbox_widget([self.control_panel, self.console_label, self.console])
         right_widget = self.qvbox_widget([self.canvas])
         splitter.addWidget(left_widget)
         splitter.addWidget(right_widget)
