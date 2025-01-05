61c34ffd9a72751b78333e58be6a66b867eab0f5
jcsteinmetz <jcsteinmetz@gmail.com>
2025-01-04 20:43:02 -0500

    permanent switch

--- backends/permanent/switch.py
+++ backends/permanent/switch.py
@@ -1,3 +1,4 @@
+import numpy as np
 from backends.permanent.component import Component
 
 class Switch(Component):
@@ -17,5 +18,15 @@ class Switch(Component):
         self.reindexed_wires = [wire - 1 for wire in self.wires]
 
     def unitary(self):
-        unitary = None
+        unitary = np.eye(self.circuit.n_wires, dtype=complex)
+        wire0 = self.reindexed_wires[0]
+        wire1 = self.reindexed_wires[1]
+        two_wire_unitary = self.two_wire_unitary()
+        unitary[wire0, wire0] = two_wire_unitary[0, 0]
+        unitary[wire0, wire1] = two_wire_unitary[0, 1]
+        unitary[wire1, wire0] = two_wire_unitary[1, 0]
+        unitary[wire1, wire1] = two_wire_unitary[1, 1]
         return unitary
+    
+    def two_wire_unitary(self):
+        return np.array([[0, 1], [1, 0]])
