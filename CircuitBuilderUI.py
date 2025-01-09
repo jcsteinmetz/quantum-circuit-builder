12ca12e70851b0dc1087d36aa87ade01d9da88e7
jcsteinmetz <jcsteinmetz@gmail.com>
2025-01-09 17:18:51 -0500

    Update CircuitBuilderUI.py

--- CircuitBuilderUI.py
+++ CircuitBuilderUI.py
@@ -1,7 +1,7 @@
 import sys
 from PySide6.QtWidgets import QApplication
-from UI.window import MainWindow
 import qdarktheme
+from UI.window import MainWindow
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
